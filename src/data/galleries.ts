8075749ec479f687963bc8c06c5700eaa44aafa0
Radoslav Ganev <radoslav-1973@outlook.com>
2026-08-21 00:27:48 +0300

    Edit config ts and add photos

--- src/data/galleries.ts
+++ src/data/galleries.ts
@@ -41,7 +41,8 @@ export const digital: GalleryPhoto[] = [
   { year: 2023, month: 8, location: "Kisumu, Kenya", file: "IMG_20230802_11.webp" },
   { year: 2023, month: 8, location: "Kisumu, Kenya", file: "IMG_20230801_18.webp" },
   { year: 2023, month: 8, location: "Airpot, Kisumu, Kenya", file: "IMG_20230801_10.webp" },
-  {year: 2023, month: 6, location: "Thai restaurant, Flic en Flac, Mauritius", file: "IMG_20230601_132.webp" }
+  { year: 2016, month: 11, location: "Thai restaurant, Flic en Flac, Mauritius", file: "img_5968.webp" },
+  { year: 2016, month: 9, location: "Thai restaurant, Flic en Flac, Mauritius", file: "img_5816.webp" },
 ];
 
 export const analog: GalleryPhoto[] = [
